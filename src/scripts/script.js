9a160b9b3e8ba0d3b4dcea62590f401a5d588e2b
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2019-04-13 16:51:45 -0500

    new plain ugly demo

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,6 @@
-console.log('%c Hello from /scripts/script.js ', 'background: lemonchiffon; border: 1px solid #fff');
-console.log('%c Welcome to tris-webpack-boilerplate! ', 'background: lavenderblush; border: 1px solid #fff');
-
-// add custom js below
+$(document).ready(function() {
+  $("a[href*='#']").click(function(t) {
+    t.preventDefault(),
+      $('html,body').animate({scrollTop: $(this.hash).offset().top}, 500)
+  })
+})
